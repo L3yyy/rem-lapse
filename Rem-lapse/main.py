96603a508843b912305255efdf7abaa8bef5fece
Laurenz <kuffiwrld999@gmail.com>
2025-09-26 13:56:25 +0800

    Update main.py
    Removed some features and added new commands

--- Rem-lapse/main.py
+++ Rem-lapse/main.py
@@ -15,31 +15,43 @@ intents = discord.Intents.default()
 intents.message_content = True
 intents.members = True
 
-bot = commands.Bot(command_prefix='y!', intents=intents)
-
-role = "kokak"
-secret_role = "kokak"
+bot = commands.Bot(command_prefix = ['rem ', 'rem'], intents = intents, case_insensitive = False)
+bot.remove_command('help')
 
 @bot.event
 async def on_ready():
+    activity = discord.Game(name = "with Ley-senpai")
+
+    await bot.change_presence(status = discord.Status.online, activity=activity)
     print(f"We're ready, {bot.user.name}")
 
+# member join function
 @bot.event
 async def on_member_join(member):
-    await member.send(f"Welcome to the server {member.name}")
+    embed = discord.Embed(
+        title = "💌  Invite Rem-lapse",
+        description = (
+            f"Oh! Welcome to the server, {member.mention}-senpai!\n\n"
+            "If you are interested on having me in your server, here's my invite link:\n"
+            "[✨ Invite me ✨](https://discord.com/oauth2/authorize?client_id=1380535770489688114&permissions=378295814145&integration_type=0&scope=bot)\n\n"
+        ),
+        color = discord.Color.blue()
+    )
+    embed.set_thumbnail(url = bot.user.avatar.url if bot.user.avatar else None)
+    await member.send(embed = embed)
 
-# assign
-@bot.command()
-async def assign(ctx):
-    role = discord.utils.get(ctx.guild.roles, name=secret_role)
-    if role:
-        await ctx.author.add_roles(role)
-        await ctx.send(f"{secret_role} has been assigned to {ctx.author.mention}")
-    else:
-        await ctx.send("Role not found.")
+# assign """DEPRECATED"""
+# @bot.command()
+# async def assign(ctx):
+#     role = discord.utils.get(ctx.guild.roles, name=secret_role)
+#     if role:
+#         await ctx.author.add_roles(role)
+#         await ctx.send(f"{secret_role} has been assigned to {ctx.author.mention}")
+#     else:
+#         await ctx.send("Role not found.")
 
 # catto command
-@bot.command()
+@bot.command(aliases = ['cat'])
 async def catto(ctx):
     url = "https://api.thecatapi.com/v1/images/search"
     
@@ -58,7 +70,7 @@ async def catto(ctx):
     await ctx.send(embed = embed)
 
 # doggo command
-@bot.command()
+@bot.command(aliases = ['dog'])
 async def doggo(ctx):
     url = "https://api.thedogapi.com/v1/images/search"
     
@@ -95,25 +107,86 @@ async def fact(ctx):
 
     await ctx.send(embed = embed)
 
-@bot.command()
-async def dm(ctx, *, msg):
-    await ctx.author.send(f"You said '{msg}'")
-
-@bot.command()
-async def helpme(ctx):
+@bot.command(aliases = ['invite'])
+async def inv(ctx):
     embed = discord.Embed(
-        title = "Here's the command menu, senpai:",
+        title = "💌  Invite Rem-lapse",
+        description = (
+            f"Oh! You want me to join your server, {ctx.author.mention}-senpai?\n\n"
+            "Aww.. Thank you so much! Here's my invite link:\n"
+            "[✨ Invite me ✨](https://discord.com/oauth2/authorize?client_id=1380535770489688114&permissions=378295814145&integration_type=0&scope=bot)\n\n"
+            "Feeling lost? Join our support server:\n"
+            "[🔎 Join here 🔎](https://discord.gg/hg7vtGfVQ8)"
+        ),
         color = discord.Color.blue()
     )
-    embed.add_field(name = "HELPFUL LINKS", value = ("[Support Server](https://discord.gg/gxM5TufB8Y)\n" "[GitHub](https://github.com/L3yyy)\n" "[Invite This Bot](https://discord.com/oauth2/authorize?client_id=1380535770489688114&permissions=378295814145&integration_type=0&scope=bot)"), inline = False)
-    embed.add_field(name = "COMMANDS", value = "List of available commands:", inline = False)
 
-    embed.add_field(name = "y!assign", value = "Assign the <@&1416039574517321831> to yourself", inline = False)
-    embed.add_field(name = "y!helpme", value = "Get a list of available commands", inline = False)
-    embed.add_field(name = "y!catto", value = "Grabs a random cat image online", inline = False)
-    embed.add_field(name = "y!doggo", value = "Grabs a random dog image online", inline = False)
-    embed.add_field(name = "y!fact", value = "Gives you a random useless fact", inline = False)
-    embed.set_footer(text = "More features coming soon!")
+    embed.set_thumbnail(url = bot.user.avatar.url if bot.user.avatar else None)
+    await ctx.send(embed = embed)
+
+# ALL COMMANDS
+@bot.command()
+async def help(ctx):
+    embed = discord.Embed(
+        title = f"Here's the command menu, senpai:",
+        description = f"Prefix `{bot.command_prefix[0]}`",
+        color = discord.Color.blue()
+    )
+    embed.add_field(name = "🔗 HELPFUL LINKS", value = ("[Support Server](https://discord.gg/gxM5TufB8Y)\n" "[GitHub](https://github.com/L3yyy)\n" "[Invite This Bot](https://discord.com/oauth2/authorize?client_id=1380535770489688114&permissions=378295814145&integration_type=0&scope=bot)"), inline = False)
+    embed.add_field(name = "⚙️ COMMANDS", value = "List of available commands:", inline = False)
+
+    embed.add_field(name = "fun", value = "Show all fun commands", inline = False )
+    embed.add_field(name = "helpme", value = "Show all available commands.", inline = False)
+    embed.add_field(name = "catto, cat", value = "Get a random picture of a cat.", inline = False)
+    embed.add_field(name = "doggo, dog", value = "Get a random picture of a dog.", inline = False)
+    embed.add_field(name = "fact", value = "Receive a random fun fact.", inline = False)
+    embed.add_field(name = "invite", value = "Get the bot's invite link.", inline = False)
+    embed.add_field(name = "serverinfo, svinfo, svinf", value = "View information about this server.", inline = False)
+    embed.set_footer(text = "✨ More features coming soon!")
+    await ctx.send(embed = embed)
+
+# FUN COMMANDS
+@bot.command()
+async def fun(ctx):
+    embed = discord.Embed(
+        title = f"Here's a list of fun commands, senpai:",
+        color = discord.Color.blue()
+    )
+    embed.add_field(name = "catto, cat", value = "Get a random picture of a cat.", inline = False)
+    embed.add_field(name = "doggo, dog", value = "Get a random picture of a dog.", inline = False)
+    embed.add_field(name = "fact", value = "Receive a random fun fact.", inline = False)
+
+    embed.set_footer(text = "type `rem help` to get a list of all available commands.")
+    
+    await ctx.send(embed = embed) 
+
+@bot.command(aliases = ['svinf', 'svinfo'])
+async def serverinfo(ctx):
+    guild = ctx.guild
+    author = ctx.author
+    boost_level = guild.premium_tier
+    boost_count = guild.premium_subscription_count
+
+    embed = discord.Embed(
+        title = f"Server info of {guild.name}",
+        color = discord.Color.blue()
+    )
+
+    embed.set_author(name=f"{author}", icon_url=author.avatar.url if author.avatar else None)
+    embed.set_thumbnail(url = guild.icon.url if guild.icon else None)
+
+    embed.add_field(name = "Server Name", value = guild.name, inline = True)
+    embed.add_field(name = "Server ID", value = guild.id, inline = True)
+    embed.add_field(name = "Owner", value = guild.owner.mention, inline = True)
+    embed.add_field(name = "Members", value = guild.member_count, inline = True)
+
+    embed.add_field(name = "Text Channels", value = len(guild.text_channels), inline = True)
+    embed.add_field(name = "Voice Channels", value = len(guild.voice_channels), inline = True)
+
+    embed.add_field(name = "Roles", value = len(guild.roles), inline = True)
+    embed.add_field(name = "Boosts & Level", value = f"{boost_level} {boost_count}", inline = True)
+    
+    embed.set_footer(text = "Server Info")
     await ctx.send(embed = embed)
 
 bot.run(token, log_handler=handler, log_level=logging.DEBUG)
